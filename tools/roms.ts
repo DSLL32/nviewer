97918a355f28f680d5d9938f2e31c46d73f24c1d
DSLL32 <329250091+DSLL32@users.noreply.github.com>
2026-09-20 20:14:08 +0000

    Prepare Jet Force Gemini viewer integration

--- tools/roms.ts
+++ tools/roms.ts
@@ -20,6 +20,7 @@ export const ROM_FILES = [
   'GoldenEye 007 (U) [!].z64',
   'Kirby 64 - The Crystal Shards (U) [!].z64',
   'Glover (U) [!].z64',
+  'Jet Force Gemini (U) [!].z64',
   'Banjo-Kazooie (U) (V1.0) [!].z64',
   "Bug's Life, A (U) [!].z64",
   "Cruis'n USA (U) (V1.0) [!].z64",
